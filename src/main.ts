ac1b4bef42b531f95e5f7d1884c40a77a117fc17
Jose Vasquez <josevasquez8365@gmail.com>
2023-09-05 17:26:53 -0400

    final fixs

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { movieEl, movieElProps } from "./movieTemplate";
 
 const movieContainer = document.getElementById("main")!;
 const searchBar = document.getElementById("searchBar") as HTMLInputElement;
+const apiKey = import.meta.env.VITE_APP_ACCESS_KEY;
+
 
 searchBar.addEventListener("keypress", (e) => {
-  e.key === "Enter" &&
+  e.key === "Enter" && searchBar.value.trim() !== "" &&
     showMovies(
       `https://api.themoviedb.org/3/search/movie?query=${searchBar.value}&include_adult=false&language=en-US&page=1`
     );
@@ -16,20 +18,21 @@ const options = {
   headers: {
     accept: "application/json",
     Authorization:
-      `Bearer ${import.meta.env.VITE_APP_ACCESS_KEY}`,
+      `Bearer ${apiKey}`,
   },
 };
 
 const getData = async (url: string) => {
   const res = await fetch(url, options);
   const { results } = await res.json();
-
+  
+  
   return results;
 };
 
 const showMovies = async (url: string) => {
   const movies = await getData(url);
-  console.log(movies);
+ 
   
   movieContainer.innerHTML = "";
   searchBar.value = "";
